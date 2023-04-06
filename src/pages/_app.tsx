90b7e0d0e41779bdaf436990953e3833f21fe5d5
Deren Somsanith <derensomsanith@gmail.com>
2023-04-06 13:38:26 -0500

    feat: add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import { type AppType } from "next/app";
-
-import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
+import { dark } from "@clerk/themes";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }} {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
