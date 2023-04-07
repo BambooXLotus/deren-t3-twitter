176eef3f11687707384c6ded0d12ff93e27ef8dd
Deren Somsanith <derensomsanith@gmail.com>
2023-04-07 15:16:37 -0500

    chore: -avatar primitive -post to include author

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,17 @@
-import { z } from "zod";
+import { clerkClient } from '@clerk/nextjs/server';
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import type { User } from '@clerk/nextjs/dist/api'
+import { TRPCError } from '@trpc/server';
+
+function filterUserForClient({ id, username, profileImageUrl }: User) {
+  return {
+    id,
+    username,
+    profileImageUrl
+  }
+}
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -11,7 +22,29 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const limit = 100
+    const posts = await ctx.prisma.post.findMany({
+      take: limit,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit
+    })).map(filterUserForClient)
+
+
+    return posts.map((post) => {
+      const author = users.find(user => user.id === post.authorId)
+      if (!author || !author.username) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Author for post not found!" })
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username
+        }
+      }
+    });
   }),
 });
